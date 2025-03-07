e17df4057967637f9ea7b062c62f8c004405c6b9
1nt3rn <152592015+1nt3rn@users.noreply.github.com>
2025-03-07 08:34:26 +0200

    Complete the exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, count) {
+    if (count < 0) return "ERROR";
+    let repeated_str = "";
+    for (let i = 1; i <= count; ++i)
+        repeated_str += str;
+    return repeated_str;
 };
 
 // Do not edit below this line
