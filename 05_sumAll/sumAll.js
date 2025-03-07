4428ef41f9fede51411466bfd63b9608b1003929
1nt3rn <152592015+1nt3rn@users.noreply.github.com>
2025-03-07 08:54:16 +0200

    Complete the exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    const isNonNegativeInteger = (num) => {
+        if (isNaN(num)) return false;
+        if (+num !== num) return false;
+        if (Math.round(num) !== num) return false;
+        if (num < 0) return false;
+        return true;
+    };
+    if (!isNonNegativeInteger(num1) || !isNonNegativeInteger(num2))
+        return "ERROR";
 
+    let sum = 0;
+    let min = num1 > num2 ? num2 : num1;
+    let max = num1 > num2 ? num1 : num2;
+    for (let i = min; i <= max; ++i)
+        sum += i;
+    return sum;
 };
 
 // Do not edit below this line
