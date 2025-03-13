abaeb29f1b476170496d2ee8fcd23632d3f24a65
1nt3rn <152592015+1nt3rn@users.noreply.github.com>
2025-03-13 18:38:04 +0200

    Complete the exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let calcAge = (person) => person.yearOfDeath === undefined ?
+        new Date().getFullYear() - person.yearOfBirth : person.yearOfDeath - person.yearOfBirth;
+    let oldest = people[0];
+    for (let i = 1; i < people.length; ++i) 
+        if (calcAge(oldest) < calcAge(people[i]))
+            oldest = people[i];
+    return oldest;
 };
 
 // Do not edit below this line
