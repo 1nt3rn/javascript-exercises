029b57ea4a22342be3cd7b65a2492e1efae71d96
1nt3rn <152592015+1nt3rn@users.noreply.github.com>
2025-03-13 18:14:14 +0200

    Complete the exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function(str) {
+    const letters = str.toLowerCase().split('')
+        .filter(letter => (letter >= 'a' && letter <= 'z') || (letter >= '0' && letter <= '9'));
+    return letters.slice().reverse().join('') === letters.join('');
 };
-
 // Do not edit below this line
 module.exports = palindromes;
