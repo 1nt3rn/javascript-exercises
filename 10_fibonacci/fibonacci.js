b5a7552a8d6c005775489a2699f0b4d9658b4ec8
1nt3rn <152592015+1nt3rn@users.noreply.github.com>
2025-03-13 18:27:06 +0200

    Complete the exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let number = +n;
+    if (number < 0) return "OOPS";
+    let arr = [0, 1, 1];
+    while (arr.length <= number)
+        arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+    return arr[n];
 };
 
 // Do not edit below this line
